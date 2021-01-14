04743aa57c447d45cb6e2ab52f4250c713564757
ZMAN <zachh@github.com>
2021-01-14 10:55:03 -0600

    added more to controllers, routes, and models

--- module-18-starter/controllers/comment-controller.js
+++ module-18-starter/controllers/comment-controller.js
@@ -21,6 +21,34 @@ const commentController = {
       })
       .catch(err => res.json(err));
   },
+  // add reply to pizza
+  addReply({params, body}, res) {
+    Comment.findOneAndUpdate(
+      {_id: params.commentId },
+      {$push: {replies: body } },
+      {new: true }
+    )
+    .then(dbPizzaData => {
+      if(dbPizzaData) {
+        res.status(404).json({message: 'No Pizza found with this id!'});
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => res.json(err));
+  },
+
+  //remove reply
+  removeReply({ params}, res) {
+    Comment.findOneAndUpdate(
+      {_id: params.commentId },
+      {$pull: {replies: body } },
+      {new: true }
+    )
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => res.json(err));
+  },
+
 
   // remove comment
   removeComment({ params }, res) {
